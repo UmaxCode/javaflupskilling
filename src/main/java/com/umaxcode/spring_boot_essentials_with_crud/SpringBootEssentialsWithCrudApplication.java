f93c32962b4f1cf3d4112b207a27671a7b57761f
umaxcode <maxwellodoom1729@gmail.com>
2024-11-13 06:09:46 +0000

    initial setup

---
+++ src/main/java/com/umaxcode/spring_boot_essentials_with_crud/SpringBootEssentialsWithCrudApplication.java
@@ -0,0 +1,13 @@
+package com.umaxcode.spring_boot_essentials_with_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialsWithCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialsWithCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umaxcode/spring_boot_essentials_with_crud/SpringBootEssentialsWithCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaxcode.spring_boot_essentials_with_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsWithCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
