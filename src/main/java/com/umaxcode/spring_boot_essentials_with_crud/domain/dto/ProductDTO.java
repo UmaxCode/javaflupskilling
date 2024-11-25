1c62baf5562ce9ebe34d1178cbb9e5f9ae09146b
umaxcode <maxwellodoom1729@gmail.com>
2024-11-25 09:43:13 +0000

    refactor: update Product dto

--- src/main/java/com/umaxcode/spring_boot_essentials_with_crud/domain/dto/ProductDTO.java
+++ src/main/java/com/umaxcode/spring_boot_essentials_with_crud/domain/dto/ProductDTO.java
@@ -1,7 +1,6 @@
 package com.umaxcode.spring_boot_essentials_with_crud.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +13,6 @@ public class ProductDTO {
 
     private Long id;
 
-    @Column(unique = true)
     private String name;
 
     private String description;
